9eb14cf66562e3d75d3cd0e5588e02dcb1fd7068
Furkan <furkanamerikaa@gmail.com>
2024-10-31 14:02:48 -0400

    Opposite number

---
+++ com.pluralsight/src/main/java/com/pluralsight/Opposite.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class Opposite {
+    public static int opposite(int number){
+        // your code here
+        int opposite = -number;
+
+        return opposite;
+
+    }
+}
+
+
+
+
